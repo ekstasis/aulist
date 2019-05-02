7f0b5a3e1dfaf390a1c07f648871f81cfb095b8a
James Baxter <straightstory@gmail.com>
2019-05-02 13:23:34 -0400

    Initial Commit

---
+++ aulist/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  aulist
+//
+//  Created by James Baxter on 5/2/19.
+//  Copyright © 2019 Spacebug Industries. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
